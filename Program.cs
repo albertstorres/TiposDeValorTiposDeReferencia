ffd3625a33327049cd46cb5f7f7effe791b5f142
Albert Sevy <albertstorres@gmail.com>
2024-01-20 20:36:22 -0300

    Finalizado o repositório da aula de tipos de valor e referência

--- Program.cs
+++ Program.cs
@@ -6,7 +6,17 @@ namespace TiposDeValorTiposDeReferencia
     {
         static void Main(string[] args)
         {
-            
+            var arr = new string[2];
+            arr[0] = "item1";
+            var arr2 = arr; // NÃO CRIA UMA CÓPIA. ARMAZENA A REFERÊNCIA DO ARR.
+
+            Console.WriteLine(arr[0]);
+            Console.WriteLine(arr2[0]);
+
+            arr[0] = "item alterado"; // ALTERA ARR E POR CONSEQUÊNCIA ARR2. ARR2 VAI APONTAR PRA O NOVO CONTEÚDO DE ARR.
+
+            Console.WriteLine(arr[0]);
+            Console.WriteLine(arr2[0]);
         }
     }
 }
